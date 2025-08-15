9b02312eb5fef71e1479771913358b41a400ba17
yasmim.silva <yasmim.silva@fpf.br>
2025-08-15 11:38:48 -0400

    MenuPrincipal Funcional

---
+++ Assets/Scripts/Menu/MenuPrincipalManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
+namespace Menu
+{
+    public class MenuPrincipalManager :  MonoBehaviour
+    {
+        //[SerializeField] private string nomeDoLevelDeJogo;
+        [SerializeField] private GameObject painelMenuInicial;
+        [SerializeField] private GameObject painelOpcoes;
+        
+        public void Jogar()
+        {
+            SceneManager.LoadScene("PrimeiraTelaJogo");
+        }
+
+        public void AbrirOpcoes()
+        {
+            painelMenuInicial.SetActive(false);
+            painelOpcoes.SetActive(true);
+        }
+
+        public void FecharOpcoes()
+        {
+            painelOpcoes.SetActive(false);
+            painelMenuInicial.SetActive(true);
+        }
+
+        public void SairDoJogo()
+        {
+            SceneManager.LoadScene("AuthScene");
+        }
+    }
+}
